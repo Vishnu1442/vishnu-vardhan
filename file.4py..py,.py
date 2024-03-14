8130ecd3a78b8f302875bd6a88d99058c6b88e56
Vishnu1442 <162986057+Vishnu1442@users.noreply.github.com>
2024-03-14 16:57:22 +0530

    Add files via upload

---
+++ file.4py..py,.py
@@ -0,0 +1,61 @@
+#------> while loop
+#------> break using while loop
+# eg:1
+#1.)iterate from 20 to 30 and break the loop in 27
+#i = 20
+#while i<31:
+#    if i == 27:
+#        break
+#    print(i)
+#    i+=1
+# 2.)
+#i = 20
+#while i<31:
+#    print(i)
+#    i+=1
+#    if i == 27:
+#        break
+
+#3.)
+#i = 20
+#while i<31:
+#    print(i)
+#    if i == 27:
+#        break
+#    i+=1
+
+#4.)
+#i = 20
+#while i<31:
+#    if i == 27:
+#        print(i)
+#        break
+#    i+=1
+
+#------> continue
+#----> Eg:1
+#i = 20
+#while i<31:
+#    if i == 27:
+#        continue
+#       print(i)
+#    i=i+1
+
+#eg:2
+#i = 20
+#while i<31:
+#    i=i+1
+#    if i == 27:
+#        continue
+#    print(i)
+
+#eg:3
+#i = 20
+#while i<31:
+#    i=i+1
+#    if i == 27:
+#        continue
+#    print(i)
+
+#eg:4
+#while try to iterate from 12 …
